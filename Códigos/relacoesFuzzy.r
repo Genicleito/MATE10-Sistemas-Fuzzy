4f9123466cd040800e83c51f654b82c6337439fb
Genicleito <genicleto@live.com>
2018-09-11 00:33:39 -0300

    Initial commit :tada:

--- Códigos/relacoesFuzzy.r
+++ Códigos/relacoesFuzzy.r
@@ -1,1 +1,9 @@
 source('funcoesPertinenciaFuzzy.r')
+
+# Variável Temperatura
+termoTempBaixa <- seq(1, 6, seq = 1)
+termoTempAlta <- seq(3, 8, seq = 1)
+
+# Variável Umidade
+termoUmidBaixa <- seq(0, 5, seq = 1)
+termoUmidAlta <- seq(2, 8, seq = 1)
