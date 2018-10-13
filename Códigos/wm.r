9f84c07595169bac9ee418840679d8419597b76e
Genicleito Gonçalves <Genicleito@users.noreply.github.com>
2018-10-12 21:29:56 -0300

    Adicionada função para adicionar regioes fuzzy no passo 1 do WM

--- Códigos/wm.r
+++ Códigos/wm.r
@@ -1,30 +1,62 @@
 # Implementação do Wang&Mendel como descrito no artigo
 
-# Lê a base de dados
-db <- read.csv('./GitHub/sistemasFuzzy/Códigos/jogarTenis.csv', sep = ',')
+# # Lê a base de dados
+# data <- read.csv('./GitHub/sistemasFuzzy/Códigos/jogarTenis.csv', sep = ',')
+
+# # A coluna 1 é um identificador do dia
+# data <- data[, -1]
+
+data <- iris
 
 # Função para pre-processar a base de dados em questão
-preProcessar <- function() {
+preProcessar <- function(db) {
   # Converte os dados em numéricos
   for(i in c(1: ncol(db))){
     db[i] = as.numeric( unlist(db[i]) )
   }
   
-  # Mistura os dados (menos a coluna 1 que é um identificador do dia)
-  db <- db[ sample( nrow(db), nrow(db)), -1]
-  
+  return (db)
+}
+
+defineTrainSet <- function(db) {
   # Coleta dados de forma aleatória para ser o conjunto de treinamento (1/3 da base)
-  db.train <- db[ (1 : ( nrow(db) * 1 / 3 )), ]
+  return (db[ (1 : ( nrow(db) * 1 / 3 )), ])
   
+}
+
+defineTestSet <- function(db) {
   # Coleta dados de forma aleatória para ser o conjunto de teste (2/3 da base)
-  db.test <- db[ (nrow(db) * 1 / 3) : ( nrow(db) ), ]
+  return(db[ (nrow(db) * 1 / 3) : ( nrow(db) ), ])
 }
 
 # Função para definir intervalos dos atributos da base de dados
 # TODO
-defineIntervalos <- function() {
+defineIntervalos <- function(db) {
+  range <- c()
+  names <- c()
   for(i in c(1 : ncol(db))) {
-    cat("Interval of ", colnames(db)[i], ": [", min(db[ , i]), ", ", max(db[ , i]), "]\n")
+    # cat("Interval of ", colnames(db)[i], ": [", min(db[ , i]), ", ", max(db[ , i]), "]\n")
+    range <- c(range, min(db[ , i]), max(db[ , i]))
+    names <- c(names, colnames(db)[i])
   }
+  result <- data.frame(matrix(data = range, nrow = 2, ncol = ncol(db), byrow = FALSE))
+  colnames(result) <- names
+  return (result)
 }
 
+
+# db é a base com os intervalos de cada variavel
+defineRegioesFuzzy <- function(db, nRegions) {
+  regions <- c()
+  nRows <- length( seq( min(db[ , 0]), max(db[ , 0]), (( (max(db[ , 0]) - min(db[ , 0]) ) / nRegions) / 2 ) ) )
+  for(i in c(1 : ncol(db))) {
+    regions <- c( regions, seq( min(db[ , i]), max(db[ , i]), (( (max(db[ , i]) - min(db[ , i]) ) / nRegions) / 2 ) ) )
+  }
+  return (data.frame(matrix(data = regions, nrow = nRows)))
+}
+
+# Chamada de funções
+data <- preProcessar(data)
+data.test <- defineTestSet(data)
+data.train <- defineTrainSet(data)
+intervalos <- defineIntervalos(data.train)
