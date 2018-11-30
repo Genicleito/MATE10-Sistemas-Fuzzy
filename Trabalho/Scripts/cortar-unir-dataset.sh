a0a0848c0afeedcf3c03ecf590d5fc99f7e4a2fe
Genicleito <genicleito@live.com>
2018-11-30 02:27:42 -0300

    finalizado script

--- Trabalho/Scripts/cortar-unir-dataset.sh
+++ Trabalho/Scripts/cortar-unir-dataset.sh
@@ -2,4 +2,5 @@ cd ../../Datasets/
 mkdir partes-enem
 sh ../Trabalho/Scripts/cortarArquivo.sh
 # split -l 250000 MICRODADOS_ENEM_2017.csv partes-enem
-Rscript ../Trabalho/Codigos/unir-partes-dataset.r
+cd partes-enem
+Rscript ../../Trabalho/Codigos/unir-partes-dataset.r
