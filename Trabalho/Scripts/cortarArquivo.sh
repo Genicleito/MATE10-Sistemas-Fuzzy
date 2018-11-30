57b03b268090c8d85ba2d38b873c2f14044a1c90
Genicleito Gonçalves <Genicleito@users.noreply.github.com>
2018-11-30 01:54:48 -0300

    Mais uma opção de cortar o dataset

--- Trabalho/Scripts/cortarArquivo.sh
+++ Trabalho/Scripts/cortarArquivo.sh
@@ -1,2 +1,3 @@
-split -l 250000 -d -a3 MICRODADOS_ENEM_2017.csv partes/
+split -l 250000 MICRODADOS_ENEM_2017.csv partes/
+# split -l 250000 -d -a3 MICRODADOS_ENEM_2017.csv partes/
 # split --bytes=60M ./MICRODADOS_ENEM_2017.csv dados/partes
