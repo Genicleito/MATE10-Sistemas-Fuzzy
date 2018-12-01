939eca0627a4b38cf2068168a032873e02fca8f4
Genicleito Gonçalves <Genicleito@users.noreply.github.com>
2018-12-01 13:31:57 -0200

    Adicionada script para remover NAs

--- Trabalho/Códigos/unir-partes-dataset.r
+++ Trabalho/Códigos/unir-partes-dataset.r
@@ -1,3 +1,5 @@
+source("pre-processamento.r")
+
 # As notas nas competencias podem nao ser interessantes pois elas já dão uma noção da nota final da redação
 competencias.redacao <- c("NU_NOTA_COMP1", "NU_NOTA_COMP2", "NU_NOTA_COMP3", "NU_NOTA_COMP4", "NU_NOTA_COMP5")
 
@@ -9,19 +11,22 @@ enem2017 <- read.csv("aa.csv", header = TRUE, sep = ";")
 atributos.dataset <- colnames(enem2017)
 
 # Atributos realmente selecionados
-atributos.selecionados <- c("NO_MUNICIPIO_RESIDENCIA", "NU_IDADE", "TP_SEXO", "TP_COR_RACA", "TP_ESCOLA", 
-                          "Q006", "Q027", "NU_NOTA_CN", "NU_NOTA_CH", "NU_NOTA_LC", "NU_NOTA_MT", 
-                          "NU_NOTA_REDACAO")
+atributos.selecionados <- c("NU_IDADE", "TP_SEXO", "TP_COR_RACA", "TP_ESCOLA", 
+                            "Q006", "Q027", "NU_NOTA_CN", "NU_NOTA_CH", "NU_NOTA_LC", "NU_NOTA_MT", 
+                            "NU_NOTA_REDACAO")
+# atributos.selecionados <- c("NO_MUNICIPIO_RESIDENCIA", "NU_IDADE", "TP_SEXO", "TP_COR_RACA", "TP_ESCOLA", 
+#                           "Q006", "Q027", "NU_NOTA_CN", "NU_NOTA_CH", "NU_NOTA_LC", "NU_NOTA_MT", 
+#                           "NU_NOTA_REDACAO")
 
 enem2017 <- enem2017[, atributos.selecionados]
 
 
 files.names <- paste("a", letters[seq(2, 26)], sep = "")
 
-dataset.atributos.relevantes <- enem2017
+dataset.atributos.relevantes <- enem2017[ -linhasComNA(enem2017) ]
 #colnames(dataset.atributos.relevantes) <- atributos.selecionados
 
-for(file.name in files.names[c(1)]) {
+for(file.name in files.names) {
   cat("Lendo arquivo...", file.name, "\n")
   file <- read.csv(file.name, header = FALSE, sep = ";")
   cat("Terminou de ler o arquivo", file.name, "\n")
@@ -30,10 +35,12 @@ for(file.name in files.names[c(1)]) {
   # Aplica o filtro de atributos selecionados
   file <- file[ , atributos.selecionados]
   
-  dataset.atributos.relevantes <- rbind(dataset.atributos.relevantes, file)
+  dataset.atributos.relevantes <- rbind(dataset.atributos.relevantes, file[ -linhasComNA(file) ])
+  # dataset.atributos.relevantes <- rbind(dataset.atributos.relevantes, file)
   
   # dataset.atributos.relevantes[, atributos.selecionados] = file[, atributos.selecionados]
 }
+
 
 a <- matrix(nrow = 2, ncol = 2)
 a[1, ] = c(11, 12)
