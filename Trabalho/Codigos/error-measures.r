90159344185857668d902fa5bc71cabc9f55b27f
pedronovaes <pedronovaesmelo@gmail.com>
2018-12-03 00:00:07 -0300

    Adicionando função para calcular o coeficiente de determinação (R2)

--- Trabalho/Codigos/error-measures.r
+++ Trabalho/Codigos/error-measures.r
@@ -23,3 +23,12 @@ mae <- function(y_true, y_pred) {
 pearson_r <- function(y_true, y_pred) {
     return(cor(y_pred, y_true, use = "complete.obs", method = "pearson"))
 }
+
+# Function to calculate Coefficient of Determination (R-Squared)
+# Input parameters:
+#   - y_true: the ground truth numeric vector.
+#   - y_pred: the predicted numeric vector.
+r_squared <- function(y_true, y_pred) {
+    library(MLmetrics)
+    return(R2_Score(y_pred, y_true))
+}
